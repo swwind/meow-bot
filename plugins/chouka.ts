65606497ac16da929ce7bac97dbbed840293d64b
swwind <swwind233@gmail.com>
2021-06-16 15:38:24 +0800

    fmt

--- plugins/chouka.ts
+++ plugins/chouka.ts
@@ -41,8 +41,8 @@ function yuanshenChouka(gid: number, uid: number, chizi: YuanshenChizi) {
 
 function appendStar(stars: number) {
   return (x: string) => {
-    return '⭐'.repeat(stars) + ' ' + x;
-  }
+    return "⭐".repeat(stars) + " " + x;
+  };
 }
 
 const YuanshenChangzhu: YuanshenChizi = {
@@ -71,30 +71,36 @@ const YuanshenHuodong1: YuanshenChizi = {
 };
 
 interface ArknightsChizi {
-  name: string,
-  star3: string[],
-  star4: string[],
-  star5: string[],
-  star6: string[],
-  star6_up: string[] | null,
-  star5_up: string[] | null,
-  star4_up: string[] | null,
+  name: string;
+  star3: string[];
+  star4: string[];
+  star5: string[];
+  star6: string[];
+  star6_up: string[] | null;
+  star5_up: string[] | null;
+  star4_up: string[] | null;
 }
 
 const ArknightsChangzhu: ArknightsChizi = {
-  name: '方舟常驻池(6-24)',
-  star6_up: '推进之王/莫斯提马'.split('/').map(appendStar(6)),
+  name: "方舟常驻池(6-24)",
+  star6_up: "推进之王/莫斯提马".split("/").map(appendStar(6)),
   star6:
-  '能天使/伊芙利特/艾雅法拉/安洁莉娜/闪灵/夜莺/星熊/塞雷娅/银灰/斯卡蒂/陈/黑/赫拉格/麦哲伦/煌/阿/刻俄柏/风笛/傀影/温蒂/早露/铃兰/棘刺/森蚺/史尔特尔/瑕光/泥岩/山/空弦/嗟峨/异客/凯尔希/卡涅利安'.split('/').map(appendStar(6)),
+    "能天使/伊芙利特/艾雅法拉/安洁莉娜/闪灵/夜莺/星熊/塞雷娅/银灰/斯卡蒂/陈/黑/赫拉格/麦哲伦/煌/阿/刻俄柏/风笛/傀影/温蒂/早露/铃兰/棘刺/森蚺/史尔特尔/瑕光/泥岩/山/空弦/嗟峨/异客/凯尔希/卡涅利安"
+      .split("/").map(appendStar(6)),
 
-  star5_up: '诗怀雅/星极/爱丽丝'.split('/').map(appendStar(5)),
-  star5: '白面鸮/凛冬/德克萨斯/芙兰卡/拉普兰德/幽灵鲨/蓝毒/白金/陨星/天火/梅尔/赫默/华法琳/临光/红/雷蛇/可颂/普罗旺斯/守林人/崖心/初雪/真理/空/狮蝎/食铁兽/夜魔/格劳克斯/送葬人/槐琥/苇草/布洛卡/灰喉/哞/惊垫/慑砂/巫恋/极境/石棉/月禾/莱恩哈特/断崖/蜜蜡/贯维/安哲拉/燧石/四月/奥斯塔/絮雨/卡夫卡/乌有/熔泉水/赤冬/绮良'.split('/').map(appendStar(5)),
+  star5_up: "诗怀雅/星极/爱丽丝".split("/").map(appendStar(5)),
+  star5:
+    "白面鸮/凛冬/德克萨斯/芙兰卡/拉普兰德/幽灵鲨/蓝毒/白金/陨星/天火/梅尔/赫默/华法琳/临光/红/雷蛇/可颂/普罗旺斯/守林人/崖心/初雪/真理/空/狮蝎/食铁兽/夜魔/格劳克斯/送葬人/槐琥/苇草/布洛卡/灰喉/哞/惊垫/慑砂/巫恋/极境/石棉/月禾/莱恩哈特/断崖/蜜蜡/贯维/安哲拉/燧石/四月/奥斯塔/絮雨/卡夫卡/乌有/熔泉水/赤冬/绮良"
+      .split("/").map(appendStar(5)),
 
   star4_up: null,
-  star4: '夜烟/远山/杰西卡/流星/白雪/清道夫/红豆/杜宾/缠丸/霜叶/慕斯/砾/暗索/末药/调香师/角峰/蛇屠箱/古米/深海色/地灵/阿消/猎蜂/格雷伊/苏苏洛/桃金娘/红云/梅/安比尔/宴/刻刀/波登可/卡达/孑/酸糖/芳汀/泡泡/杰克/松果/豆苗/深靛'.split('/').map(appendStar(4)),
+  star4:
+    "夜烟/远山/杰西卡/流星/白雪/清道夫/红豆/杜宾/缠丸/霜叶/慕斯/砾/暗索/末药/调香师/角峰/蛇屠箱/古米/深海色/地灵/阿消/猎蜂/格雷伊/苏苏洛/桃金娘/红云/梅/安比尔/宴/刻刀/波登可/卡达/孑/酸糖/芳汀/泡泡/杰克/松果/豆苗/深靛"
+      .split("/").map(appendStar(4)),
 
-  star3: '芬/香草/翎羽/玫兰莎/卡缇/米格鲁/克洛丝/炎熔/芙蓉/安赛尔/史都华德/梓兰/空爆/月见夜/班点/泡普卡'.split('/').map(appendStar(3)),
-}
+  star3: "芬/香草/翎羽/玫兰莎/卡缇/米格鲁/克洛丝/炎熔/芙蓉/安赛尔/史都华德/梓兰/空爆/月见夜/班点/泡普卡".split("/")
+    .map(appendStar(3)),
+};
 
 function arknightsChouka(gid: number, uid: number, chizi: ArknightsChizi) {
   const last_six_star = storage.getOr(`${gid}.${uid}.lastsix`, 0);
@@ -113,15 +119,15 @@ function arknightsChouka(gid: number, uid: number, chizi: ArknightsChizi) {
   let res: string[] = [];
   if (star === 6) {
     if (Math.random() < 0.5 && chizi.star6_up) res = chizi.star6_up;
-    else res=  chizi.star6;
+    else res = chizi.star6;
   }
   if (star === 5) {
     if (Math.random() < 0.5 && chizi.star5_up) res = chizi.star5_up;
-    else res=  chizi.star5;
+    else res = chizi.star5;
   }
   if (star === 4) {
     if (Math.random() < 0.5 && chizi.star4_up) res = chizi.star4_up;
-    else res=  chizi.star4;
+    else res = chizi.star4;
   }
   if (star === 3) {
     res = chizi.star3;
