ae4ce3ad161db8d641ae4ac9996789c3570c8010
swwind <swwind233@gmail.com>
2021-06-17 14:45:42 +0800

    remove unused code

--- test.ts
+++
@@ -1,5 +0,0 @@
-import { deserializeFeed } from './deps.ts';
-          const response = await fetch('https://blog.swwind.me/feeds');
-          const xml = await response.text();
-          const { feed } = await deserializeFeed(xml, { outputJsonFeed: true });
-          console.log(feed);
