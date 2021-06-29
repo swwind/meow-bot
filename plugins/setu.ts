3a4233d2a51b82475ce15ff8bf13364a1453a291
swwind <swwind233@gmail.com>
2021-06-29 17:04:49 +0800

    update setu

--- plugins/setu.ts
+++ plugins/setu.ts
@@ -1,68 +1,119 @@
 import { DeserializedMessage, IHelperReply, IPlugin } from "../types.ts";
 import { messageText } from "../utils.ts";
 
-const helpText = "来点涩图";
-
-/*
-function fetchSetu(timeout = 10000) {
+function fetchTimeout(url: string, timeout = 5000) {
   return Promise.race([
-    new Promise<string>((_, reject) => setTimeout(reject, timeout)),
-    (async () => {
-      const response = await fetch(
-        "https://api.lovemiku.online/miku/?master=1",
-      );
-      const json = await response.json();
-      const url: string = json?.url;
-      if (!url || typeof url !== "string") {
-        throw new Error("API error");
-      }
-      return url;
-    })(),
+    new Promise<Response>((_, reject) => setTimeout(reject, timeout)),
+    fetch(url),
   ]);
 }
-*/
+
+async function fetchLovemikuSetu() {
+  const data = await (await fetchTimeout('https://api.lovemiku.online/miku/?master=1')).json();
+  const url = data?.url;
+  if (typeof url !== 'string') {
+    throw new Error('Not found');
+  }
+  return url;
+}
+
+async function fetchLoliconSetu() {
+  const data = await (await fetchTimeout('https://api.lolicon.app/setu/v2')).json();
+  const url = data?.data?.[0]?.urls?.original;
+  if (typeof url !== 'string') {
+    throw new Error('Not found');
+  }
+  return url;
+}
+
+async function fetchLocalSetu() {
+  const setus: string[] = [];
+  for await (const entry of Deno.readDir("/root/setu")) {
+    if (entry.isFile && /\.(?:jpe?g|png)$/i.test(entry.name)) {
+      setus.push(entry.name);
+    }
+  }
+  if (!setus.length) {
+    throw new Error();
+  }
+  const setu = setus[Math.floor(Math.random() * setus.length)];
+  return `file:///root/setu/${setu}`;
+}
+
+async function fetchIslandwind233Setu() {
+  return 'https://islandwind233.pro/ZY/API/getimgapi.php';
+}
+
+async function fetchToubiecSetu() {
+  const data = await (await fetchTimeout('https://acg.toubiec.cn/random.php?ret=json')).json();
+  const url = data?.[0]?.imgurl;
+  if (typeof url !== 'string') {
+    throw new Error('Not found');
+  }
+  return url;
+}
+
+async function fetchTouhouSetu() {
+  return 'https://img.paulzzh.tech/touhou/random';
+}
+
+const setuApis = [{
+  name: 'local',
+  fetch: fetchLocalSetu,
+}, {
+  name: 'lovemiku',
+  fetch: fetchLovemikuSetu,
+}, {
+  name: 'lolicon',
+  fetch: fetchLoliconSetu,
+}, {
+  name: 'islandwind233',
+  fetch: fetchIslandwind233Setu,
+}, {
+  name: 'toubiec',
+  fetch: fetchToubiecSetu,
+}, {
+  name: 'touhou',
+  fetch: fetchTouhouSetu,
+}];
+
+const helpText = `来点 $type? 涩图\ntype = { ${setuApis.map(({ name }) => name).join(', ')} }`;
 
 export const SetuPlugin: IPlugin = {
   name: "setu",
   helpText,
-  async onGroupMessage(
+  async onAllMessage(
     helper: IHelperReply,
     _gid: number,
     _uid: number,
     message: DeserializedMessage,
   ) {
     const text = messageText(message.messageChain);
-    if (text === "来点涩图") {
+    const result = /^来点([\s\S]*)(?:涩|色)图$/gi.exec(text);
+    if (result) {
+      const type = result[1].trim();
       try {
-        let setus: string[] = [];
-        for await (const entry of Deno.readDir("/root/setu")) {
-          if (entry.isFile && /\.(?:jpe?g|png)$/i.test(entry.name)) {
-            setus.push(entry.name);
+        if (type === '') {
+          const api = setuApis[Math.floor(Math.random() * setuApis.length)];
+          helper.reply([{
+            type: 'Image',
+            url: await api.fetch(),
+          }]);
+          return;
+        }
+        for (const api of setuApis) {
+          if (api.name === type) {
+            helper.reply([{
+              type: 'Image',
+              url: await api.fetch(),
+            }]);
+            return;
           }
         }
-        if (!setus.length) {
-          throw new Error();
-        }
-        const setu = setus[Math.floor(Math.random() * setus.length)];
-        helper.reply([{
-          type: "Image",
-          url: `file:///root/setu/${setu}`,
-        }]);
+        helper.reply('/help setu');
       } catch (e) {
-        helper.reply("获取涩图失败");
+        helper.reply(Math.random() < 0.01 ? '别冲了' : '获取失败，可能图源炸了');
       }
-      return;
-    }
-  },
-  onFriendMessage(
-    helper: IHelperReply,
-    _uid: number,
-    message: DeserializedMessage,
-  ) {
-    const text = messageText(message.messageChain);
-    if (text === "来点涩图") {
-      helper.reply("别冲了");
-      return;
     }
   },
 };
